ccb3292f6bfdfbc58538c80cb6d9c148e30cb509
Mashiro Shiina <33735917+NarutoInstinct@users.noreply.github.com>
2019-09-19 14:38:55 +0200

    routing hooman

--- PGD Prototype2/Assets/Scripts/HumanController.cs
+++ PGD Prototype2/Assets/Scripts/HumanController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class HumanController : MonoBehaviour
 {
+    public GameObject chosenRoute;
+    public float speed = 5f;
+    int followPointIndex = 0;
+    float originalY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(chosenRoute.transform.childCount);
+        originalY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, chosenRoute.transform.GetChild(followPointIndex).position, step);
+
+        transform.position = new Vector3(transform.position.x, originalY, transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Point")
+        {
+            if (followPointIndex + 1 < chosenRoute.transform.childCount)
+            {
+                followPointIndex++;
+            }
+
+        }
     }
 }
