2feafbd3bf7d6eab16ba2d62a5c50acba6eea8a7
aclesh <39330979+aclesh@users.noreply.github.com>
2019-09-19 14:57:21 +0200

    ScareMeter beginnings

---
+++ PGD Prototype2/Assets/Scripts/ScareMeter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScareMeter : MonoBehaviour
+{
+    private int scare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scare = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
