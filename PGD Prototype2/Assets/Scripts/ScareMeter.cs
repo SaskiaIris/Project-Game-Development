8ef49c10a5001a306ef5a5c6dfe9b30d2314d403
aclesh <39330979+aclesh@users.noreply.github.com>
2019-09-25 13:30:10 +0200

    Functional ScareMeter

--- PGD Prototype2/Assets/Scripts/ScareMeter.cs
+++ PGD Prototype2/Assets/Scripts/ScareMeter.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class ScareMeter : MonoBehaviour
 {
-    private int scare;
+    public int scare;
+    private RectTransform balkWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         scare = 0;
+        balkWidth=GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (scare<=100)
+        {
+            balkWidth.sizeDelta = new Vector2(scare*2, balkWidth.sizeDelta.y);
+        } 
     }
 }
