8dfcbb7d036a092750aefaf28dcbb15bf0a6d65e
ikarian7 <scott.marchant7@gmail.com>
2019-09-25 20:30:22 +0200

    punten toegevoegd en verlies van punten als je klikt

--- PGD Prototype2/Assets/Scripts/Button.cs
+++ PGD Prototype2/Assets/Scripts/Button.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour
 {
+    int puntenAantal;
+    public Text puntText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        puntenAantal = 100;
+        setPuntenText();
     }
 
     public void Click()
     {
-        Debug.Log("Iris = gay");
+        puntenAantal -= 5;
+        Debug.Log(puntenAantal);
+        setPuntenText();
+    }
+
+    public void setPuntenText()
+    {
+        puntText.text = "Punten: " + puntenAantal.ToString();
     }
 
     // Update is called once per frame
