73a300d597ef124497e67392274ea8682d241f0f
Cas <37177482+Zerrelyn@users.noreply.github.com>
2019-09-25 20:03:57 +0200

    Added trap class and snake trap

---
+++ Project Game Development/Assets/Prototype2/Trap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Trap : ScriptableObject
+{
+    public string Name = "New trap";
+    public string Description = "It's a trap";
+    public Sprite Icon;
+    
+    protected GameObject target;
+    protected Vector3 position;
+    
+    public virtual void Initialize(Vector3 position, GameObject target)
+    {
+        position = this.position;  target = this.target;
+    }
+
+    public virtual void CheckForTarget() { }
+
+    public abstract void TriggerTrap();
+}
