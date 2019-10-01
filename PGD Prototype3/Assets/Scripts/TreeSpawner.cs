b67594c2124af7dca3967ecc7abdcafd2399ce4b
Cas <37177482+Zerrelyn@users.noreply.github.com>
2019-10-01 15:03:31 +0200

    Added random tree spawns

---
+++ PGD Prototype3/Assets/Scripts/TreeSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tree, treeContainer;
+
+    [SerializeField]
+    private float spawnRangeX = 20, spawnRangeZ = 20;
+
+    [SerializeField]
+    private int amountOfTrees = 100;
+
+    private void Awake()
+    {
+        for(int i = 0; i < amountOfTrees; i++)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+            Quaternion spawnRotation = Quaternion.Euler(0, Random.Range(0, 360f), 0);
+            Instantiate(tree, spawnPos, spawnRotation, treeContainer.transform);
+        }
+    }
+}
