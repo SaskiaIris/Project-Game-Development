bb95913a3eb94c641177a244ac297d60b7fbd446
aclesh <39330979+aclesh@users.noreply.github.com>
2019-10-02 15:16:40 +0200

    CameraStopsPlayerMovement

--- PGD Prototype3/Assets/Scripts/CameraChange.cs
+++ PGD Prototype3/Assets/Scripts/CameraChange.cs
@@ -7,25 +7,28 @@ public class CameraChange : MonoBehaviour
     public Cinemachine.CinemachineFreeLook Camera;
     public Cinemachine.CinemachineFreeLook Camera2;
 
-    private int oldPriorityCamera;
-    private int oldPriorityCamera2;
     // Start is called before the first frame update
     void Start()
     {
-        int oldPriorityCamera = GameObject.Find("ThirdPersonCamera").GetComponent<Cinemachine.CinemachineFreeLook>().Priority;
     }
-
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Z))
         {
-            Camera.Priority = Camera2.Priority;
-            Camera2.Priority = oldPriorityCamera;
-
-            oldPriorityCamera = Camera.Priority;
+            Camera.Priority = 9;
+            Camera2.Priority = 10;
+            GameObject.Find("YBot").GetComponent<MovementInput>().enabled = false;
         }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            Camera.Priority = 10;
+            Camera2.Priority = 9;
+            GameObject.Find("YBot").GetComponent<MovementInput>().enabled = true;
+        }
+
     }
 }
 
