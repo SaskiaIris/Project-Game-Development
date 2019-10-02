ec55981e080069db55857c914a47a67c45db1519
Cas <37177482+Zerrelyn@users.noreply.github.com>
2019-10-02 12:44:01 +0200

    Added gathering

---
+++ PGD Prototype3/Assets/Scripts/GatherableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatherableObject : MonoBehaviour
+{
+    [SerializeField]
+    private float health;
+
+    public int ResourceAmount { get; private set; }
+    
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+    }
+}
